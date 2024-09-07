3560805ce235840c9091b97c3fedfda0f7c2714f
hunter87ff <huntergaming702@gmail.com>
2024-09-08 02:33:57 +0530

    minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,27 +6,30 @@ const Footer: React.FC = () => {
         <footer className="bg-gray-900 text-white py-8">
             <div className="container mx-auto flex flex-col items-center">
                 <h2 className="text-2xl font-bold mb-4">LostLister</h2>
-                <p className="text-center text-gray-400 mb-6">
-                    Description about the page goes here.
+                <p className="text-center text-gray-400 mb-16">
+                Don't Let Memories Fade. Find Them Back.
                 </p>
-                <div className="flex space-x-4">
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        About
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        Contact
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        Login
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        Privacy
-                    </a>
-                </div>
-                <p className="text-gray-400 mt-6">
+                <div className="flex space-x-4 footer-content">
+                    <div className="footer-tab">
+                        <a href="#" className="text-gray-400 hover:text-white">
+                            About
+                        </a>
+                        <a href="#" className="text-gray-400 hover:text-white">
+                            Contact
+                        </a>
+                        <a href="#" className="text-gray-400 hover:text-white">
+                            Login
+                        </a>
+                        <a href="#" className="text-gray-400 hover:text-white">
+                            Privacy
+                        </a>
+                    </div>
+                    <p className="text-gray-400 mt-6">
                     {/* &copy; {new Date().getFullYear()} LostLister. All rights reserved. */}
                     Made with ❤️ by hunter87.
-                </p>
+                    </p>
+                </div>
+
             </div>
         </footer>
     );
