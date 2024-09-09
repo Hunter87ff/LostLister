dd0e64900f3c536470baf9c3803af6a23751b2da
hunter87ff <huntergaming702@gmail.com>
2024-09-10 00:08:24 +0530

    im sorry, but every time i'm fixing something, im also making an issue too!! ;)

--- src/components/home/LatestPost.jsx
+++ src/components/home/LatestPost.jsx
@@ -12,10 +12,10 @@ return  (
         <h2>Item : {item.title}</h2>
         <p>Details : {item.details}</p>
         <p>Resolved : {item.resolved?"Resolved":"Not Resolved"}</p>
-        {/* <p>Owner : {item.owner||"notFound"}</p> */}
+        <p>Owner : {item.owner||"notFound"}</p>
         <p>Founded By : {item.founded_by}</p>
         <p>Founded At : {item.founded_at}</p>
-        {/* <p>Spam Reports : {item.reports}</p> */}
+        <p>Spam Reports : {item.reports}</p>
         <button className='bg-green-600 ml-0 w-50' onClick={()=>{console.log("claimed : " + item.id)}}>Claim</button>
         <button className="bg-red-400 text-white ml-0 w-50">Report</button>
     </div>
